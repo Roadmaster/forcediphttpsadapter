425600bf7e706cae09a4da1b32a5662995b95a0d
Daniel Manrique <roadmr@ubuntu.com>
2017-12-14 22:48:11 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 
 PACKAGE_NAME = 'forcediphttpsadapter'
-PACKAGE_VERSION = '1.0.0'
+PACKAGE_VERSION = '1.0.1'
 AUTHOR = 'Roadmaster'
 EMAIL = 'daniel@tomechangosubanana.com'
 URL = 'https://github.com/Roadmaster/forcediphttpsadapter'
