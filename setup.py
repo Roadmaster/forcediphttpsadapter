89a331597cce312c39ff33c02c29663a08674049
retornam <retornam@users.noreply.github.com>
2023-05-31 21:24:51 -0700

    Resolve DeprecationWarning: distutils
    distutils will be deprecated in python 3.12 [1]
    This commit replaces distutils.StrictVersion with
    the Version class from the packaging egg.
    
    [1] https://docs.python.org/3/whatsnew/3.10.html#distutils-deprecated

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ connecting via SSL to a web service without running into SNI trouble.
 from setuptools import setup, find_packages
 
 
-PACKAGE_NAME = 'forcediphttpsadapter'
-PACKAGE_VERSION = '1.0.2'
-AUTHOR = 'Roadmaster'
-EMAIL = 'daniel@tomechangosubanana.com'
-URL = 'https://github.com/Roadmaster/forcediphttpsadapter'
+PACKAGE_NAME = "forcediphttpsadapter"
+PACKAGE_VERSION = "1.0.2"
+AUTHOR = "Roadmaster"
+EMAIL = "daniel@tomechangosubanana.com"
+URL = "https://github.com/Roadmaster/forcediphttpsadapter"
 
 
 setup(
@@ -24,19 +24,15 @@ setup(
     url=URL,
     # classifiers=CLASSIFIERS,
     # platforms=PLATFORMS,
-    provides=['adapters'],
-    install_requires=['requests'],
+    provides=["adapters"],
+    install_requires=["packaging", "requests"],
     # dependency_links=dependency_links,
-
     packages=find_packages(),
     # include_package_data=True,
     # package_data=package_data,
-
-    download_url='{}/archive/master.zip'.format(URL),
+    download_url="{}/archive/master.zip".format(URL),
     # keywords=KEYWORDS,
     # scripts=scripts,
-
     # entry_points={},
-
     zip_safe=False,
 )
